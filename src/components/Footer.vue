62aac0c6c179019465950c84b2f1b4037d2aad58
thallesyasmim <thallesgabriel1307@gmail.com>
2020-12-23 21:17:35 -0300

    Finished Footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+  <footer>
+      Studies <span>Vue.js</span> By Thalles Gabriel
+  </footer>
+</template>
+
+<script>
+export default {
+    name: 'Footer'
+}
+</script>
+
+<style scoped>
+    footer {
+        padding: 50px 0;
+        font-family: monospace;
+        font-size: 20px;
+        font-weight: 700;
+        color: #2c3e50;
+    }
+
+    footer span {
+        color: #18222c;
+    }
+</style>>
