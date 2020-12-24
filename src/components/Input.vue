e9764c9f4c30cf0e0bc59b24d57efe57a029af93
thallesyasmim <thallesgabriel1307@gmail.com>
2020-12-23 22:08:39 -0300

    Style Input

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -14,5 +14,13 @@ export default {
 </script>
 
 <style scoped>
-
+    input {
+        border: 2px solid #2c3e50;
+        border-radius: 5px;
+        margin: 7px 0 20px;
+        padding: 10px 10px; 
+        font-weight: 600;
+        color: #18222c;
+        box-shadow: 5px 5px 15px rgba(0, 0, 0, 0.25);
+    }
 </style>
