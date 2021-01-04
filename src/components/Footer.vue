425177b79cc367b94d61c7be2b8d907ce2050f06
thallesyasmim <thallesgabriel1307@gmail.com>
2021-01-04 20:32:30 -0300

    Add directive v-else

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,8 +3,11 @@
         <footer v-if="footer && !directive"> <!-- Directive: Attribute that does not exist in HTML -->
             Studies <span>{{ footer.framework }}</span> By {{ footer.author }}
         </footer>
-        <footer v-else-if="!footer && directive">
+        <footer id="one" v-else-if="!footer && directive">
             {{ directive }} 
+        </footer>
+         <footer v-else>
+             directive: <span>v-else</span> 
         </footer>
         <footer v-show="!footer && !directive">
             <span>v-show</span> keeps the element on a page with "display: none;" 
@@ -24,7 +27,7 @@ export default {
 
 <style scoped>
     footer {
-        padding: 50px 0;
+        padding: 20px 0;
         font-family: monospace;
         font-size: 20px;
         font-weight: 700;
@@ -33,5 +36,9 @@ export default {
 
     footer span {
         color: #18222c;
+    }
+
+    #one {
+        margin-top: 40px;
     }
 </style>>
