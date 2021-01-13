724252d1ad1007744575431aa7bbde42b17c2795
thallesyasmim <thallesgabriel1307@gmail.com>
2021-01-13 19:15:37 -0300

    New Component "Form"

---
+++ src/components/Form.vue
@@ -0,0 +1,29 @@
+<template>
+  <form>
+     <h1>Register</h1>
+     <input type="text" placeholder="Add Circle">
+  </form>
+</template>
+
+<script>
+    export default {
+        name: 'Form'
+    }
+</script>
+
+<style scoped>
+    h1 {
+        color: #2c3e50;
+        margin-bottom: 10px;
+    }
+
+    input {
+        border: 2px solid #2c3e50;
+        border-radius: 5px;
+        margin: 7px 5px 20px;
+        padding: 10px 10px; 
+        font-weight: 600;
+        color: #18222c;
+        box-shadow: 5px 5px 15px rgba(0, 0, 0, 0.25);
+    }
+</style>
